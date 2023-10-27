5fd4c617fa58058c04f3e4438a1724d846b151ff
KibeThuku <kibethuku@gmail.com>
2023-10-27 23:02:47 +0300

    1_0x11_elif_stat_crashcoursepdf.py commit

---
+++ 0x11. elif statement/1_0x11_elif_stat_crashcoursepdf.py
@@ -0,0 +1,16 @@
+"""
+• Admission for anyone under age 4 is free.
+• Admission for anyone between the ages of 4 and 18 is $25.
+• Admission for anyone age 18 or older is $40.
+"""
+
+age = 12
+
+if age < 4:
+    print("Your admission cost is $0.")
+elif age > 4:
+    print("Your admission cost is $25.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40.")
---
+++ 0x11. elif statement/README.md
@@ -0,0 +1,8 @@
+readme file for 0x11 elif statements
+
+Author : Ken Kibe Thuku
+
+Source:
+1. https://www.udemy.com/course/the-data-science-course-complete-data-science-bootcamp/learn/lecture/10772704#overview
+
+2. 
